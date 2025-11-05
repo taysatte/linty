458994e0e3744ac287a427e973443be249664b8a
taysatte <taylor.sattenfield@gmail.com>
2025-11-05 12:35:09 -0600

    style: adjust padding in CodeEditor and EditorControls components for improved layout and visual consistency

--- src/components/editor/CodeEditor.tsx
+++ src/components/editor/CodeEditor.tsx
@@ -72,7 +72,7 @@ const CodeEditor = ({
   };
 
   return (
-    <Card className="shadow-lg h-full w-full gap-2 pt-2">
+    <Card className="shadow-lg h-full w-full gap-2 pt-4">
       <div className="flex flex-col h-full">
         <EditorControls
           language={language}
