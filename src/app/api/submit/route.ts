33930486aba2527b3170870abd9811bbe99ff261
taysatte <taylor.sattenfield@gmail.com>
2025-12-23 22:27:27 -0600

    fix: update import statement for prisma to use default import for consistency

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import { getPuzzleById, getUserSubmission } from "@/lib/puzzle";
 import { isSupportedLanguage } from "@/lib/languageVersions";
